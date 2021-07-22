7456c33b14b265301e6d7c89a20ef60d766be8dc
Quénard <vincent.quenard@free.fr>
2021-07-22 12:11:41 +0200

    ajout vérifications champs noms et prénoms >2 caractères sinon alert

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,29 +13,90 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const buttonClose = document.querySelector(".close");
 //const formClose = document.querySelector(".bground")
+
+//sélection des balises du formulaires
+const prenom = document.getElementById('first');
+const nom = document.getElementById('last');
+const messagerie = document.getElementById('email');
+const naissance = document.getElementById('birthdate');
+const nombre_tournois = document.getElementById('quantity');
+const ville = document.getElementsByName('location')
 const focuscgu = document.getElementById('checkbox1');
 
-// launch modal event
+// listener bouton ouverture formulaire
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// fonction pour faire apparaitre le formulaire
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// Close modal event
+// listener fermeture formulaire au clic sur la croix
 buttonClose.addEventListener("click",closeModal);
 
 //Close modal click out event
 //formClose.addEventListener("click",closeModal);
 
-//Close modal form
+//Fonction de fermeture formulaire au clic sur la croix
 function closeModal() {
   modalbg.style.display = "none";
 
 }
+//Vérifications formulaires
 
 
+//verification champ prénom non vide
+prenom.addEventListener("change",checkprenom);
+function checkprenom()
+{
+   if (prenom.value.length <2)
+ {
+  alert('Vous devez entrer plus de 2 charactères');
+  return false;
+ }
+ else
+ {
+  return true;
+ }
+}
+
+//verification champ nom non vide
+nom.addEventListener("change",checknom);
+function checknom()
+{
+   if (nom.value.length <2)
+ {
+  alert('Vous devez entrer plus de 2 charactères');
+  return false;
+ }
+ else
+ {
+  return true;
+ }
+}
+
+//Vérification si une des villes est cochée
+ville.addEventListener("click", checkville);
+function checkville()
+{
+  console.log(ville)
+ if (ville.checked)
+ {
+  return true;
+ }
+ else
+ {
+  alert('Vous devez cocher une ville');
+  return false;
+ }
+}
+
+
+
+
+
+
+//vérification si cgu cochée
 focuscgu.addEventListener("click",checkcgu);
 function checkcgu()
 {
@@ -49,7 +110,6 @@ function checkcgu()
   return false;
  }
 }
-
 
 
 /*squelette pour s'inspirer pour onsubmit : y mettre tous les champs et boutons ?
@@ -67,4 +127,10 @@ function valider(){
   }
 }*/
 
+//idée validation button submit .....qui ne ....fonctionne pas
+const envoyer = document.querySelector(".btn-submit");
 
+envoyer.addEventListener('click', (e) => {
+  e.preventDefault();
+  alert('merci !');
+})
